b5d9262336b63faa8750a2fd346d6e421156c489
Samar AYADI <ayadisamardev@gmail.com>
2023-03-17 02:04:10 +0100

    First commit todo pingucoder

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { TodoItem } from './model/todoItem';
+import { TodoList } from './model/todoList';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todo';
+  private list = new TodoList('Samar AYADI', [
+    new TodoItem('Go to Gym', true),
+    new TodoItem('Go for Run'),
+    new TodoItem('Go to Cinema'),
+  ]);
+
+  showComplete: boolean = false;
+
+  get username(): string {
+    return this.list.user;
+  }
+
+  get itemcount(): number {
+    return this.list.items.filter(item => !item.complete).length;
+  }
+
+  get items(): readonly TodoItem[] {
+    return this.list.items.filter(item => this.showComplete || !item.complete);
+  } 
+
+  addItem(itemTask: string) {
+    if(itemTask != '') {
+      this.list.addItems(itemTask);
+    }
+  }
 }
---
+++ src/app/model/todoItem.ts
@@ -0,0 +1,11 @@
+export class TodoItem {
+    constructor(public task:string, public complete:boolean = false) {
+        // statement
+    }
+}
+
+// User Samar
+
+// got to gym --> true
+// buy milk --> false
+// study --> false
